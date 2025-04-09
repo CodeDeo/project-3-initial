a58b6b86590dbff9204ab2065f18821adb0c1d11
CodeDeo <Dem2139259@maricopa.edu>
2025-04-09 14:57:14 -0700

    Finished project 3

---
+++ src/main/java/Project3/Container.java
@@ -0,0 +1,11 @@
+package Project3;
+
+public interface Container <T> extends Iterable<T> {
+    boolean isEmpty();
+
+    int size();
+
+    void add(T item);
+}
+
+
--- src/test/java/com/example/iterable/BagTest.java
+++ src/test/java/com/example/iterable/BagTest.java
@@ -1,5 +1,6 @@
 package com.example.iterable;
 
+import Project3.Bag;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
